727c5ad45da59c7f891a5f39c314bdc334a02131
Cyril Mizzi <cyril@lesiteimmo.com>
2019-09-09 10:27:48 +0200

    fix: update tmux configuration and ssh theme

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -124,4 +124,8 @@ function homestead() {
     ( cd /home/data/homestead && vagrant $* )
 }
 
+function certdates() {
+	echo | openssl s_client -servername $1 -connect $1:443 2>/dev/null | openssl x509 -noout -dates
+}
+
 # vim: ft=zsh
